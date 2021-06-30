4133a9e7dc304fa9067ad10f7cd72dbb82e7e302
Romual Naibaho <romualnaibaho26@gmail.com>
2021-06-30 22:56:42 +0700

    js

---
+++ tugas19-b.js
@@ -0,0 +1,14 @@
+function bintang(){
+
+    var str = "*****";
+
+    console.log(str.repeat(1));
+    console.log(str.repeat(5));
+    console.log(str.repeat(9));
+    console.log(str.repeat(14));
+    console.log(str.repeat(9));
+    console.log(str.repeat(5));
+    console.log(str.repeat(1));
+}
+
+bintang();
---
+++ tugas19.js
@@ -0,0 +1,9 @@
+function ulang(){
+    var str = "semangat...";
+
+    str = str.repeat(5);
+
+    return str;
+}
+
+console.log(ulang());
